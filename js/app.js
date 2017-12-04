e356a9fada405b14365bffeca75501201d8d9717
Hiago Patrick <hiagoptk@hotmail.com>
2017-12-04 21:45:51 -0200

    segundo texte de versionamento

--- js/app.js
+++ js/app.js
@@ -18,3 +18,21 @@ largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 85
 
 var body = document.body;
 body.style.color = 'red';
+
+function largestOfFour(arr) {
+  // I can do this!
+  var a = arr;
+  var b = 0;
+   for (var i = 0; i < a.length; i++) {
+     if (a[i].length > b) {
+           b = a[i].length;
+       }
+     return b;
+     }
+
+
+
+
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
